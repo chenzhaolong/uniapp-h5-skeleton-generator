b76c56e3531056dff6dc506e2748daeb4788fd0c
chenzhaolong <1332588507@qq.com>
2021-09-10 02:06:53 +0800

    开发autoWatchPlugin

--- pkg/webpack-uniapp-auto-insert-plugin/src/autoWatchPlugin.js
+++ pkg/webpack-uniapp-auto-insert-plugin/src/autoWatchPlugin.js
@@ -1,13 +1,35 @@
 /*
 * @file 骨架屏内容自动监听骨架屏文件变动；
 */
+const child_process = require('child_process');
 
 class AutoWatchPlugin {
     constructor(options) {
+        this.watchDir = options.watchDir || '';
+        this.watchFiles = options.watchFiles || [];
+    }
+
+    apply(compiler) {
+        if (!this.watchDir && !this.watchFiles) {
+            this.error('watchDir or watchFiles is wrong in UniAppInsertSkeletonPlugin');
+        }
+
+        compiler.hooks.watchRun.tapAsync('watchRun', (watch, cb) => {
+            let filesChange = watch.compiler.watchFileSystem.watcher.mtimes;
+        });
+    }
+
+    generatorSsrJson() {
 
     }
 
-    apply(compiler) {}
+    error(msg) {
+        if (msg instanceof Error) {
+            throw msg;
+        } else {
+            throw new Error(msg);
+        }
+    }
 }
 
 module.exports = {
